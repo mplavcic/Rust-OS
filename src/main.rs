4e32eff6576b5d7e1201f922ea41d882387a54ed
mateo <mateo.plava@gmail.com>
2020-11-27 11:42:30 +0100

    Print "Hello World!" using VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}    
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // turns off Rust's name mangling, so that it is easier to link to
 pub extern "C" fn _start() -> ! { // this function is adhere to the C calling convention
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
