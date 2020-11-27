1c612edc4a1df191e37096092fd22b16b97a1713
mateo <mateo.plava@gmail.com>
2020-11-27 10:30:00 +0100

    Add [no_std] and [no_main] attributes and entry point
    * Since we cant have any OS dependent libs we disable std lib
    	and also since we dont have a runtime system we dont
    	have main() so we define an entry point
    * We define panic handler since we dont link std lib

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std] // standard library depends on host OS and libc, so we disable it
+#![no_main] // we dont have a runtime environment to call it so we disable main
+
+use core::panic::PanicInfo;
+
+#[panic_handler] // used to define the behavior of panic! in #![no_std] app
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}    
+}
+
+#[no_mangle] // turns off Rust's name mangling, so that it is easier to link to
+pub extern "C" fn _start() -> ! { // this function is adhere to the C calling convention
+    loop {}
+}
