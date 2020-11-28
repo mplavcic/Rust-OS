681a6b2adbf7edd15e578d5438fa0dc431001cad
mateo <mateo.plava@gmail.com>
2020-11-28 22:07:01 +0100

    Simple vga_buffer test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std] // standard library depends on host OS and libc, so we disable it
 #![no_main] // we dont have a runtime environment to call it so we disable main
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler] // used to define the behavior of panic! in #![no_std] app
@@ -8,16 +9,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}    
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // turns off Rust's name mangling, so that it is easier to link to
 pub extern "C" fn _start() -> ! { // this function is adhere to the C calling convention
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
     loop {}
 }
